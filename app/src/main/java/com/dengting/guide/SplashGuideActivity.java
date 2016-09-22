14e2f0b3c313bab71998dd8f6677327b42dd0b96
jsonwuyong <sundywuyong@outlook.com>
2016-09-22 23:34:38 +0800

    增加引导页面

---
+++ app/src/main/java/com/dengting/guide/SplashGuideActivity.java
@@ -0,0 +1,15 @@
+package com.dengting.guide;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.dengting.lovedengting.R;
+
+public class SplashGuideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_guide);
+    }
+}
