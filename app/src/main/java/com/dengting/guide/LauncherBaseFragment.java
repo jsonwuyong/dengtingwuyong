f7a89b95ae0de4e6a35d7618dd12247ed9166d2e
jsonwuyong <sundywuyong@outlook.com>
2016-09-26 23:18:28 +0800

    增加引导也没fragment

---
+++ app/src/main/java/com/dengting/guide/LauncherBaseFragment.java
@@ -0,0 +1,13 @@
+package com.dengting.guide;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Fragment抽象类
+ * @author dengting
+ * 
+ */
+public abstract class LauncherBaseFragment extends Fragment{
+	public abstract void  startAnimation();
+	public abstract void  stopAnimation();
+}
--- app/src/main/java/com/dengting/guide/SplashGuideActivity.java
+++ app/src/main/java/com/dengting/guide/SplashGuideActivity.java
@@ -1,15 +1,54 @@
 package com.dengting.guide;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
+import com.dengting.adpter.BaseFragmentAdapter;
 import com.dengting.lovedengting.R;
 
-public class SplashGuideActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class SplashGuideActivity extends FragmentActivity {
+
+    private GuideViewPager vPager;
+    private List<LauncherBaseFragment> list = new ArrayList<LauncherBaseFragment>();
+    private BaseFragmentAdapter adapter;
+    private ImageView[] tips;
+    private int currentSelect;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_guide);
+        //初始化控件
+        initWigthView();
+    }
+
+    private void initWigthView() {
+        ViewGroup viewGroup = (ViewGroup) findViewById(R.id.viewGroup);
+        tips = new ImageView[3];
+        for(int i = 0;i<tips.length;i++){
+            ImageView imageView = new ImageView(this);
+            imageView.setLayoutParams(new LayoutParams(10, 10));
+            if(i == 0){
+                imageView.setBackgroundResource(R.mipmap.page_indicator_focused);
+            }else{
+                imageView.setBackgroundResource(R.mipmap.page_indicator_unfocused);
+            }
+            tips[i]=imageView;
+
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(new ViewGroup.LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT));
+            layoutParams.leftMargin = 20;//设置点点点view的左边距
+            layoutParams.rightMargin = 20;//设置点点点view的右边距
+            viewGroup.addView(imageView,layoutParams);
+
+
+        }
+
+
     }
 }
